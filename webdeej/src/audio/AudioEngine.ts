e2029427291dc62a4a5917b37aec81059acb3d42
MantasGrusa <mantas.grusauskas@gmail.com>
2026-02-12 19:25:42 +0100

    change audio engine

--- webdeej/src/audio/AudioEngine.ts
+++ webdeej/src/audio/AudioEngine.ts
@@ -1,9 +1,25 @@
 export class AudioEngine {
     private static instance: AudioEngine;
     private audioContext: AudioContext;
-    
+
     private constructor() {
         this.audioContext = new AudioContext();
     }
-  
+    
+    static getInstance(): AudioEngine {
+        if (!AudioEngine.instance) {
+            AudioEngine.instance = new AudioEngine();
+        }   
+        return AudioEngine.instance;
+    }
+
+    get context(): AudioContext {
+        return this.audioContext;
+    }
+
+    async resume(){
+        if (this.audioContext.state === 'suspended') {
+            await this.audioContext.resume();
+        }
+    }
 }
